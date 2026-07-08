a5a404be592ccdc39da20aba4d980ea3c7a4d34e
sharonvarghesee <sanjusharon8@gmail.com>
2026-07-08 13:31:42 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
-import sharonAsset from "@/assets/sharon-profile.png.asset.json";
-import awsCloudAsset from "@/assets/aws-cloud.jpg.asset.json";
-import signLanguageAsset from "@/assets/sign-language.png.asset.json";
+import sharonAsset from "@/assets/sharon-profile.png.json";
+import awsCloudAsset from "@/assets/aws-cloud.jpg.json";
+import signLanguageAsset from "@/assets/sign-language.png.json";
 const sharonImg = sharonAsset.url;
 
 export const Route = createFileRoute("/")({
